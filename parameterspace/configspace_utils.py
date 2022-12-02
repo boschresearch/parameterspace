d92a4c9ceab77b1dba4f3671584d43a6e8520945
Lukas Großberger <Lukas.Grossberger@de.bosch.com>
2022-12-02 15:35:39 +0100

    Feature/init from config space (closes #36) (#48)
    * add basic ps from cs initialization #36 
    * bump version to 0.7.22
    
    Signed-off-by: Grossberger Lukas (CR/AIR2.2) <Lukas.Grossberger@de.bosch.com>

---
+++ parameterspace/configspace_utils.py
@@ -0,0 +1,206 @@
+"""Initialize a `ParameterSpace` from a `ConfigSpace` JSON dictionary."""
+
+from typing import List, Optional, Tuple
+
+import numpy as np
+
+import parameterspace as ps
+from parameterspace.condition import Condition
+from parameterspace.utils import verify_lambda
+
+
+def _escape_parameter_name(name: str) -> str:
+    """Replace colons with underscores.
+
+    Colons are incompatible as ParameterSpace parameter names.
+    """
+    return name.replace(":", "_")
+
+
+def _get_condition(
+    conditions: List[dict], configspace_parameter_name: str
+) -> Optional[Condition]:
+    """Construct a lambda function that can be used as a ParameterSpace condition from a
+    ConfigSpace conditions list given a specific target parameter name.
+
+    NOTE: The `configspace_parameter_name` here needs to match the original name in
+    `ConfigSpace`, not the one transformed with `_escape_parameter_name`.
+    """
+    condition = Condition()
+
+    varnames = []
+    function_texts = []
+    for cond in conditions:
+        if cond["child"] == configspace_parameter_name:
+            parent = _escape_parameter_name(cond["parent"])
+            varnames.append(parent)
+            # The representation is used because it quotes strings.
+            if cond["type"] == "IN":
+                function_texts.append(f"{parent} in {tuple(cond['values'])}")
+            elif cond["type"] == "EQ":
+                function_texts.append(f"{parent} == {repr(cond['value'])}")
+            elif cond["type"] == "NEQ":
+                function_texts.append(f"{parent} != {repr(cond['value'])}")
+            elif cond["type"] == "GT":
+                function_texts.append(f"{parent} > {repr(cond['value'])}")
+            elif cond["type"] == "LT":
+                function_texts.append(f"{parent} < {repr(cond['value'])}")
+            else:
+                raise NotImplementedError(f"Unsupported condition type {cond['type']}")
+
+    if not varnames:
+        return condition
+
+    function_text = " and ".join(function_texts)
+    verify_lambda(variables=varnames, body=function_text)
+    # pylint: disable=eval-used
+    condition_function = eval(f"lambda {', '.join(varnames)}: {function_text}")
+    # pylint: enable=eval-used
+
+    condition.function_texts.append(function_text)
+    condition.varnames.append(varnames)
+    condition.all_varnames |= set(varnames)
+    condition.functions.append(condition_function)
+
+    return condition
+
+
+def _convert_for_normal_parameter(
+    log: bool, lower: Optional[float], upper: Optional[float], mu: float, sigma: float
+) -> Tuple[float, float, float, float]:
+    """Convert bounds and prior mean/std from `ConfigSpace` parameter dictionary with
+    normal prior to `ParameterSpace` compatible values.
+
+    Args:
+        log: Are we on a log scale?
+        lower: Optional lower bound in the original space (required when `log=True`)
+        upper: Optional upper bound in the original space (required when `log=True`)
+        mu: Mean of the `ConfigSpace` normal distribution
+        sigma: Standard deviation of the `ConfigSpace` normal distribution
+
+    Returns:
+        Transformed lower bound, upper bound, mean and standard deviation
+
+    Raises:
+        Value error when log is True but bounds are missing.
+    """
+    if lower is None or upper is None:
+        if log:
+            raise ValueError(
+                "Please provide bounds, when using a log transform with a normal prior."
+            )
+        lower = mu - 4 * sigma
+        upper = mu + 4 * sigma
+
+    if log:
+        log_upper, log_lower = np.log(upper), np.log(lower)
+        log_interval_size = log_upper - log_lower
+        mean = (mu - log_lower) / log_interval_size
+        std = sigma / log_interval_size
+    else:
+        interval_size = upper - lower
+        mean = (mu - lower) / interval_size
+        std = sigma / interval_size
+
+    return lower, upper, mean, std
+
+
+def parameterspace_from_configspace_dict(configspace_dict: dict) -> ps.ParameterSpace:
+    """Create `ParameterSpace` instance from a `ConfigSpace` JSON dictionary.
+
+    Note, that `ParameterSpace` does not support regular, non-truncated normal priors
+    and will thus translate an unbounded normal prior to a normal truncated at +/- 4
+    sigma. Also, constant parameters are represented as categoricals with a single value
+    that are fixed to said value.
+
+    Args:
+        configspace_dict: The dictionary based on a `ConfigSpace` JSON representation.
+
+    Returns:
+        A `ParameterSpace` instance.
+
+    Raises:
+        NotImplementedError in case a given parameter type or configuration is not
+        supported.
+    """
+    space = ps.ParameterSpace()
+
+    for param_dict in configspace_dict["hyperparameters"]:
+        param_name = _escape_parameter_name(param_dict["name"])
+        condition = _get_condition(configspace_dict["conditions"], param_dict["name"])
+        if param_dict["type"] == "uniform_int":
+            space._parameters[param_name] = {
+                "parameter": ps.IntegerParameter(
+                    name=param_name,
+                    bounds=(param_dict["lower"], param_dict["upper"]),
+                    transformation="log" if param_dict["log"] else None,
+                ),
+                "condition": condition,
+            }
+
+        elif param_dict["type"] == "categorical":
+            space._parameters[param_name] = {
+                "parameter": ps.CategoricalParameter(
+                    name=param_name,
+                    values=param_dict["choices"],
+                    prior=param_dict.get("weights", None),
+                ),
+                "condition": condition,
+            }
+
+        elif param_dict["type"] in ["constant", "unparametrized"]:
+            space._parameters[param_name] = {
+                "parameter": ps.CategoricalParameter(
+                    name=param_name,
+                    values=[param_dict["value"]],
+                ),
+                "condition": condition,
+            }
+            space.fix(**{param_name: param_dict["value"]})
+
+        elif param_dict["type"] in ["normal_float", "normal_int"]:
+            parameter_class = (
+                ps.ContinuousParameter
+                if param_dict["type"] == "normal_float"
+                else ps.IntegerParameter
+            )
+            lower_bound, upper_bound, mean, std = _convert_for_normal_parameter(
+                log=param_dict["log"],
+                lower=param_dict.get("lower", None),
+                upper=param_dict.get("upper", None),
+                mu=param_dict["mu"],
+                sigma=param_dict["sigma"],
+            )
+            space._parameters[param_name] = {
+                "parameter": parameter_class(
+                    name=param_name,
+                    bounds=(lower_bound, upper_bound),
+                    prior=ps.priors.TruncatedNormal(mean=mean, std=std),
+                    transformation="log" if param_dict["log"] else None,
+                ),
+                "condition": condition,
+            }
+
+        elif param_dict["type"] == "uniform_float":
+            space._parameters[param_name] = {
+                "parameter": ps.ContinuousParameter(
+                    name=param_name,
+                    bounds=(param_dict["lower"], param_dict["upper"]),
+                    transformation="log" if param_dict["log"] else None,
+                ),
+                "condition": condition,
+            }
+
+        elif param_dict["type"] == "ordinal":
+            space._parameters[param_name] = {
+                "parameter": ps.OrdinalParameter(
+                    name=param_name,
+                    values=param_dict["sequence"],
+                ),
+                "condition": condition,
+            }
+
+        else:
+            raise NotImplementedError(f"Unsupported type {param_dict['type']}")
+
+    return space
---
+++ tests/test_configspace_utils.py
@@ -0,0 +1,395 @@
+import json
+
+import numpy as np
+import pytest
+from ConfigSpace import (
+    ConfigurationSpace,
+    EqualsCondition,
+    Float,
+    GreaterThanCondition,
+    InCondition,
+    LessThanCondition,
+    Normal,
+    NotEqualsCondition,
+)
+from ConfigSpace.read_and_write import json as cs_json
+from scipy.stats import truncnorm as scipy_truncnorm
+
+from parameterspace.configspace_utils import parameterspace_from_configspace_dict
+from parameterspace.priors.categorical import Categorical as CategoricalPrior
+from parameterspace.priors.truncated_normal import (
+    TruncatedNormal as TruncatedNormalPrior,
+)
+from parameterspace.transformations.log_zero_one import (
+    LogZeroOneInteger as LogZeroOneIntegerTransformation,
+)
+
+CS_CONDITIONS_JSON = """{
+  "hyperparameters": [
+    {
+      "name": "alpha",
+      "type": "uniform_float",
+      "log": true,
+      "lower": 0.001,
+      "upper": 1095.0,
+      "default": 1.0
+    },
+    {
+      "name": "booster",
+      "type": "categorical",
+      "choices": [
+        "gblinear",
+        "gbtree",
+        "dart"
+      ],
+      "default": "gblinear",
+      "probabilities": null
+    },
+    {
+      "name": "lambda",
+      "type": "uniform_float",
+      "log": true,
+      "lower": 0.0009118819655545162,
+      "upper": 1096.6331584284585,
+      "default": 1.0
+    },
+    {
+      "name": "nrounds",
+      "type": "uniform_int",
+      "log": true,
+      "lower": 8,
+      "upper": 2980,
+      "default": 122
+    },
+    {
+      "name": "repl",
+      "type": "uniform_int",
+      "log": false,
+      "lower": 1,
+      "upper": 10,
+      "default": 6
+    },
+    {
+      "name": "max_depth",
+      "type": "uniform_int",
+      "log": false,
+      "lower": 3,
+      "upper": 10,
+      "default": 3
+    },
+    {
+      "name": "rate_drop",
+      "type": "uniform_float",
+      "log": false,
+      "lower": 0.0,
+      "upper": 1.0,
+      "default": 0.2
+    }
+  ],
+  "conditions": [
+    {
+      "child": "max_depth",
+      "parent": "booster",
+      "type": "IN",
+      "values": [
+        "dart",
+        "gbtree"
+      ]
+    },
+    {
+      "child": "rate_drop",
+      "parent": "booster",
+      "type": "EQ",
+      "value": "dart"
+    }
+  ],
+  "forbiddens": [],
+  "python_module_version": "0.4.18",
+  "json_format_version": 0.2
+}"""
+
+
+def _cs_to_dict(cs: ConfigurationSpace) -> dict:
+    return json.loads(cs_json.write(cs))
+
+
+def test_conditions_and_log_transform():
+    cs_dict = json.loads(CS_CONDITIONS_JSON)
+    space = parameterspace_from_configspace_dict(cs_dict)
+    assert len(space) == 7
+    assert space.has_conditions()
+    assert space.check_validity(
+        {
+            "alpha": 1.0,
+            "booster": "gbtree",
+            "lambda": 1.0,
+            "nrounds": 122,
+            "repl": 6,
+            "max_depth": 3,
+        }
+    )
+    assert isinstance(
+        space._parameters["nrounds"]["parameter"]._transformation,
+        LogZeroOneIntegerTransformation,
+    )
+
+    _s = space.copy()
+    _s.fix(booster="dart")
+    assert len(_s.sample()) == 7
+
+    _s = space.copy()
+    _s.fix(booster="gblinear")
+    assert len(_s.sample()) == 5
+
+    alpha = cs_dict["hyperparameters"][0]
+    assert alpha["name"] == "alpha"
+    bounds = [alpha["lower"], alpha["upper"]]
+    assert list(space._parameters["alpha"]["parameter"].bounds) == bounds
+
+    booster = cs_dict["hyperparameters"][1]
+    assert booster["name"] == "booster"
+    assert space._parameters["booster"]["parameter"].values == booster["choices"]
+
+
+def test_continuous_with_normal_prior():
+    cs_dict = json.loads(
+        """{
+          "name": "myspace",
+          "hyperparameters": [
+            {
+              "name": "p",
+              "type": "normal_float",
+              "log": false,
+              "mu": 8.0,
+              "sigma": 10.0,
+              "default": 6.2
+            }
+          ],
+          "conditions": [],
+          "forbiddens": [],
+          "python_module_version": "0.6.0",
+          "json_format_version": 0.4
+        }
+        """
+    )
+    space = parameterspace_from_configspace_dict(cs_dict)
+    assert len(space) == 1
+
+    param = space.get_parameter_by_name("p")["parameter"]
+    assert isinstance(param._prior, TruncatedNormalPrior)
+
+    samples = np.array([space.sample()["p"] for _ in range(10_000)])
+    assert abs(samples.mean() - cs_dict["hyperparameters"][0]["mu"]) < 0.1
+    assert abs(samples.std() - cs_dict["hyperparameters"][0]["sigma"]) < 0.2
+
+
+def test_continuous_with_log_normal_prior_and_no_bounds_raises():
+    cs = ConfigurationSpace(
+        space={
+            "p": Float(
+                "p",
+                default=0.1,
+                log=True,
+                distribution=Normal(1.0, 0.6),
+            ),
+        },
+    )
+    with pytest.raises(ValueError):
+        parameterspace_from_configspace_dict(_cs_to_dict(cs))
+
+
+def test_continuous_with_log_normal_prior():
+    mu = 1.0
+    sigma = 0.6
+    cs_dict = json.loads(
+        f"""{{
+          "name": "myspace",
+          "hyperparameters": [
+            {{
+              "name": "p",
+              "type": "normal_float",
+              "lower": 1e-5,
+              "upper": 1e-1,
+              "log": true,
+              "mu": {mu},
+              "sigma": {sigma},
+              "default": 1.1
+            }}
+          ],
+          "conditions": [],
+          "forbiddens": [],
+          "python_module_version": "0.6.0",
+          "json_format_version": 0.4
+        }}
+        """
+    )
+    space = parameterspace_from_configspace_dict(cs_dict)
+    assert len(space) == 1
+
+    param = space.get_parameter_by_name("p")["parameter"]
+    assert isinstance(param._prior, TruncatedNormalPrior)
+
+    samples = np.array([space.sample()["p"] for _ in range(10_000)])
+
+    a, b = (np.log(param.bounds) - mu) / sigma
+    expected_mean = scipy_truncnorm.stats(a, b, loc=mu, scale=sigma, moments="m")
+    assert abs(np.log(samples).mean() - expected_mean) < 0.1
+
+    expected_var = scipy_truncnorm.stats(a, b, loc=mu, scale=sigma, moments="v")
+    assert abs(np.log(samples).var() - expected_var) < 0.1
+
+
+def test_integer_with_normal_prior():
+    cs_dict = json.loads(
+        """{
+          "name": "myspace",
+          "hyperparameters": [
+            {
+              "name": "p",
+              "type": "normal_int",
+              "log": false,
+              "mu": 8.0,
+              "sigma": 5.0,
+              "default": 2
+            }
+          ],
+          "conditions": [],
+          "forbiddens": [],
+          "python_module_version": "0.6.0",
+          "json_format_version": 0.4
+        }
+        """
+    )
+    space = parameterspace_from_configspace_dict(cs_dict)
+    assert len(space) == 1
+
+    param = space.get_parameter_by_name("p")["parameter"]
+    assert isinstance(param._prior, TruncatedNormalPrior)
+
+    samples = np.array([space.sample()["p"] for _ in range(10_000)])
+
+    assert abs(samples.mean() - cs_dict["hyperparameters"][0]["mu"]) < 0.1
+    assert abs(samples.std() - cs_dict["hyperparameters"][0]["sigma"]) < 0.3
+
+
+def test_categorical_with_custom_probabilities():
+    cs_dict = json.loads(
+        """{
+          "name": "myspace",
+          "hyperparameters": [
+            {
+              "name": "c",
+              "type": "categorical",
+              "choices": [
+                "red",
+                "green",
+                "blue"
+              ],
+              "default": "blue",
+              "weights": [
+                2,
+                1,
+                1
+              ]
+            }
+          ],
+          "conditions": [],
+          "forbiddens": [],
+          "python_module_version": "0.6.0",
+          "json_format_version": 0.4
+        }"""
+    )
+    space = parameterspace_from_configspace_dict(cs_dict)
+    assert len(space) == 1
+
+    param = space.get_parameter_by_name("c")["parameter"]
+    assert isinstance(param._prior, CategoricalPrior)
+    reference_weights = np.array(cs_dict["hyperparameters"][0]["weights"])
+    assert np.all(
+        param._prior.probabilities == reference_weights / reference_weights.sum()
+    )
+
+
+def test_equals_condition():
+    cs = ConfigurationSpace({"a": [1, 2, 3], "b": (1.0, 8.0)})
+    cond = EqualsCondition(cs["b"], cs["a"], 1)
+    cs.add_condition(cond)
+
+    space = parameterspace_from_configspace_dict(_cs_to_dict(cs))
+    assert len(space) == 2
+
+    _s = space.copy()
+    _s.fix(a=1)
+    assert len(_s.sample()) == 2
+
+    _s = space.copy()
+    _s.fix(a=2)
+    assert len(_s.sample()) == 1
+
+
+def test_not_equals_condition():
+    cs = ConfigurationSpace({"a": [1, 2, 3], "b": (1.0, 8.0)})
+    cond = NotEqualsCondition(cs["b"], cs["a"], 1)
+    cs.add_condition(cond)
+
+    space = parameterspace_from_configspace_dict(_cs_to_dict(cs))
+    assert len(space) == 2
+
+    _s = space.copy()
+    _s.fix(a=2)
+    assert len(_s.sample()) == 2
+
+    _s = space.copy()
+    _s.fix(a=1)
+    assert len(_s.sample()) == 1
+
+
+def test_less_than_condition():
+    cs = ConfigurationSpace({"a": (0, 10), "b": (1.0, 8.0)})
+    cond = LessThanCondition(cs["b"], cs["a"], 5)
+    cs.add_condition(cond)
+
+    space = parameterspace_from_configspace_dict(_cs_to_dict(cs))
+    assert len(space) == 2
+
+    _s = space.copy()
+    _s.fix(a=4)
+    assert len(_s.sample()) == 2
+
+    _s = space.copy()
+    _s.fix(a=6)
+    assert len(_s.sample()) == 1
+
+
+def test_greater_than_condition():
+    cs = ConfigurationSpace({"a": (0, 10), "b": (1.0, 8.0)})
+    cond = GreaterThanCondition(cs["b"], cs["a"], 5)
+    cs.add_condition(cond)
+
+    space = parameterspace_from_configspace_dict(_cs_to_dict(cs))
+    assert len(space) == 2
+
+    _s = space.copy()
+    _s.fix(a=6)
+    assert len(_s.sample()) == 2
+
+    _s = space.copy()
+    _s.fix(a=4)
+    assert len(_s.sample()) == 1
+
+
+def test_in_condition():
+    cs = ConfigurationSpace({"a": (0, 10), "b": (1.0, 8.0)})
+    cond = InCondition(cs["b"], cs["a"], [1, 2, 3, 4])
+    cs.add_condition(cond)
+
+    space = parameterspace_from_configspace_dict(_cs_to_dict(cs))
+    assert len(space) == 2
+
+    _s = space.copy()
+    _s.fix(a=2)
+    assert len(_s.sample()) == 2
+
+    _s = space.copy()
+    _s.fix(a=5)
+    assert len(_s.sample()) == 1
