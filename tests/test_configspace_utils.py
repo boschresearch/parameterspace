c028e13fe87b64e9caefe903e67cca4eeaff2778
Grossberger Lukas (CR/AIR2.2) <Lukas.Grossberger@de.bosch.com>
2023-01-18 11:04:37 +0100

    rename lmbd in test to lambda_
    Signed-off-by: Grossberger Lukas (CR/AIR2.2) <Lukas.Grossberger@de.bosch.com>

--- tests/test_configspace_utils.py
+++ tests/test_configspace_utils.py
@@ -46,7 +46,7 @@ CS_CONDITIONS_JSON = """{
       "probabilities": null
     },
     {
-      "name": "lmbd",
+      "name": "lambda_",
       "type": "uniform_float",
       "log": true,
       "lower": 0.0009118819655545162,
@@ -122,7 +122,7 @@ def test_conditions_and_log_transform():
         {
             "alpha": 1.0,
             "booster": "gbtree",
-            "lmbd": 1.0,
+            "lambda_": 1.0,
             "nrounds": 122,
             "repl": 6,
             "max_depth": 3,
